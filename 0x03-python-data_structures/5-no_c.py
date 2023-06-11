fca5dfb5bc45f749c3af863345ead59657e326e3
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-06-11 15:43:46 +0000

    python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.translate(str.maketrans("", "", "cC")))
+    x = ""
+    y = ""
+    z = "cC"
+    table = str.maketrans(x, y, z)
+    return my_string.translate(table)
