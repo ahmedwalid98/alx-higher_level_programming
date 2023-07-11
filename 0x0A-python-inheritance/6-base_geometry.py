4513d603e29d30951b67f72b232307b3e16f970f
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-07-11 00:05:53 +0000

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class BaseGeometry
+"""
+class BaseGeometry:
+    """
+    methods: area
+    raises: esception
+    """
+    def area(slef):
+        """raise exception"""
+        raise Exception("area() is not implemented")
