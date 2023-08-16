f65fbf0a0d135b41fa17c2ac336015d87a8b539b
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-08-16 21:06:18 +0300

    more sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- create Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
+-- create table
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256),
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES TO states(id)
+)
