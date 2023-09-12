018c642d07a67b6cf20b51563eda289a158bc7e2
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-09-12 18:17:12 +0300

    js objects

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        console.log('X');
+      }
+      console.log('\n');
+    }
+  }
+}
+
+module.exports = Rectangle;
