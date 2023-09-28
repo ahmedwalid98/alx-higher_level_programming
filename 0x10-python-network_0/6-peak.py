48ca093416c1f8a5e67de474bf3ab4e66c24ee7e
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-09-28 22:48:52 +0300

    find peak number

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,20 +10,20 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    peak_list = []
-    for i in range(0, len(list_of_integers)):
-        if i == 0:
-            if list_of_integers[i] >= list_of_integers[i + 1]:
-                peak_list.append(list_of_integers[i])
-            else:
-                continue
-        elif i == len(list_of_integers) - 1:
-            if list_of_integers[i] >= list_of_integers[i - 1]:
-                peak_list.append(list_of_integers[i])
-            else:
-                continue
-        elif list_of_integers[i] >= list_of_integers[i + 1] \
-                and list_of_integers[i] >= list_of_integers[i - 1]:
-            peak_list.append(list_of_integers[i])
+    l = 0
+    r = len(list_of_integers)
+    mid = l + (r - l) / 2
+    mid = int(mid)
+    if r == 1:
+        return list_of_integers[0]
+    if r == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid + 1] \
+        and list_of_integers[mid] >= list_of_integers[mid - 1]:
+        return list_of_integers[mid]
+    
+    if mid > 0 and list_of_integers[mid + 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[mid + 1:])
 
-    return max(peak_list)
+    if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[:mid - 1])
