51981d254ed639e33ce56bd67ee8cea602c37b16
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-09-28 22:00:39 +0300

    sending post with params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sending post req with body
-curl -s --data "email: test@gmail.com&subject: I will always be here for PLD" "$1"
+curl -s --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"
