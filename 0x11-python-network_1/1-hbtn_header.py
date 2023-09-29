f7205ca5219d35bfa294e4236a70a15ad283bca8
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-09-29 22:43:30 +0300

    get the id header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
 - of
 """
+import urllib.request
+import sys
 
 
 if __name__ == '__main__':
