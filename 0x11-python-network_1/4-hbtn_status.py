637b641bf05d871f007548be16d7f7a9cd2669c3
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-09-30 00:03:37 +0300

    fetch using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    fetch url using requests
+"""
+import requests
+
+
+if __name__ == '__main__':
+    body = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response')
+    print('\t- type: {}'.format(type(body.text)))
+    print('\t- content: {}'.format(body.text))
