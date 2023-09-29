0b217ccb4c2a8aa574360c34eea3077598aa8fa6
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-09-29 22:41:31 +0300

    get the id header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ import sys
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of 
+- of
 """
 
 
@@ -12,4 +12,3 @@ if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         print(response.getheader('X-Request-Id'))
-    
