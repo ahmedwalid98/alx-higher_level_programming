6c93ac920bc64ce862dd40ec62d0d0bf8d3faa26
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-10-25 01:23:12 +0300

    web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2], (err, res, body) => {
-  if (!err) console.log(res.statusCode);
+  if (!err) console.log(`code: ${res.statusCode}`);
 });
