9d27f7d196636605146c5d54dbcc26b855d0975b
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-10-25 01:33:48 +0300

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const request = require('request');
-request.get(`https://swapi-api.alx-tools.com/api/films/${argv[2]}`)
-  .on('response', (res) => {
-    console.log(res.title);
+request.get(`https://swapi-api.alx-tools.com/api/films/${argv[2]}`,
+  (error, response, body) => {
+    if (!error) console.log(JSON.parse(body).title);
   });
